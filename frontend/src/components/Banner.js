8ebdd94df2135e2eddbd953e868ba35612142ba9
RaghavGupta-01 <raghav010304@gmail.com>
2024-01-28 19:00:35 +0530

    fixed banner

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -6,9 +6,10 @@ const Banner = () =>
 {
     return (
         <a href="http://192.168.77.84/" target="_blank" className='banner-to-crispr'>
+              <h1 className='banner-heading'>Visit our Server</h1>
             <Typography  gutterBottom className='link'>
-                <h1>Visit our Server</h1>
-                <img src= {CrisperLogo} height={200} width={200}/>
+              
+                <img className='banner-logo' src= {CrisperLogo} height={200} width={200}/>
             </Typography>
         </a>            
     )
