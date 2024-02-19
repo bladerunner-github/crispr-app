629616be19a46224984b6d707818566bf5fcd379
RaghavGupta-01 <raghav010304@gmail.com>
2024-02-19 21:59:35 +0530

    send mail to councellor on buzzer form submit

--- frontend/src/components/Buzzer_Form.js
+++ frontend/src/components/Buzzer_Form.js
@@ -1,101 +1,104 @@
 import React, { Component } from "react";
+import { Form, Col, Button } from 'react-bootstrap';
 import "../styles/Buzzer_Form.css"
 
+const initialFormData = Object.freeze({
+  username: "",
+  email: "",
+  mobile: "",
+  query: "",
+  date: "",
+  time: ""
+});
+
 class Buzzer_Form extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      rollNo: "",
-      phoneNumber: "",
-      picture: null,
-      message: "",
-    };
-  }
+
+  state = {
+    formData: initialFormData
+  };
+
+  sendFeedback = (serviceID, templateId, variables) => {
+    window.emailjs.send(
+      serviceID, templateId,
+      variables
+    ).then(res => {
+      console.log('Email successfully sent!')
+    })
+      .catch(err => console.error('There has been an Error.', err))
+  };
 
   handleChange = (e) => {
-    const { name, value, type } = e.target;
+    const { name, value } = e.target;
     this.setState({
-      [name]: type === "file" ? e.target.files[0] : value,
+      formData: {
+        ...this.state.formData,
+        [name]: value.trim()
+      }
     });
   };
 
   handleSubmit = (e) => {
+    alert(`Message sent successfully`);
+    const templateId = 'template_ihh4hyw';
+    const serviceID = "service_xig2bbs";
+    this.sendFeedback(serviceID, templateId, { from_name: this.state.formData.name, mobile: this.state.formData.mobile, message_html: this.state.formData.query, email: this.state.formData.email, date: this.state.formData.date, time: this.state.formData.time });
+
+    console.log(this.state.formData);
+    this.state = this.state.formData;
     e.preventDefault();
-    this.setState((prevState) => ({
-        name: "",
-        rollNo: "",
-        phoneNumber: "",
-        picture: null,
-        message: "",
-      }));
-    alert("Form submitted");
   };
 
   render() {
-    return (<section>
-     
-      <div className="form-title">Form Title</div>
-      <div className="form_container">
-        <form className="form" onSubmit={this.handleSubmit}>
-          
-          <div className="input-box">
-            <label>Name:</label>
-            <input
-              type="text"
-              name="name"
-              value={this.state.name}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
+    return (
+      <section>
+        <div className="buzzer_form">
+          <Form>
+            <div className="buzzer_col">
+              <Form.Group controlId="formGridName">
+                <Form.Label>Name*</Form.Label>
+                <Form.Control onChange={this.handleChange} name="name" type="name" placeholder="Name" required/>
+              </Form.Group>
+            </div>
 
-          <div className="input-box">
-            <label>Roll Number:</label>
-            <input
-              type="text"
-              name="rollNo"
-              value={this.state.rollNo}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
+            <div className="buzzer_col">
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Email*</Form.Label>
+                <Form.Control onChange={this.handleChange} name="email" type="email" placeholder="Enter email" required/>
+              </Form.Group>
+            </div>
 
-          <div className="input-box">
-            <label>Phone Number:</label>
-            <input
-              type="tel"
-              name="phoneNumber"
-              value={this.state.phoneNumber}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
+            <div className="buzzer_col">
+              <Form.Group as={Col} controlId="formGridMobile">
+                <Form.Label>Mobile no.*</Form.Label>
+                <Form.Control onChange={this.handleChange} name="mobile" placeholder="" required/>
+              </Form.Group>
+            </div>
 
-          <div className="input-box">
-            <label>Upload Picture:</label>
-            <input
-              type="file"
-              name="picture"
-              onChange={this.handleChange}
-              accept=".jpg, .jpeg, .png"
-            />
-          </div>
+            <div className="buzzer_col">
+              <Form.Group as={Col} id="formGridQuery">
+                <Form.Label>Message</Form.Label>
+                <Form.Control onChange={this.handleChange} name="query" as="textarea" rows={3} />
+              </Form.Group>
+            </div>
 
-          <div className="input-box">
-            <label>Message:</label>
-            <br></br>
-           
-            <input type="textarea" name="message"
-              value={this.state.message}
-              onChange={this.handleChange}></input>
-          </div>
+            <div className="buzzer_col">
+              <Form.Group as={Col} id="formGridQuery">
+                <Form.Label>Date</Form.Label>
+                <Form.Control onChange={this.handleChange} name="date" type="date" rows={3} required/>
+              </Form.Group>
+            </div>
 
-          <div>
-            <button type="submit">Submit</button>
-          </div>
-        </form>
-      </div>
+            <div className="buzzer_col">
+              <Form.Group as={Col} id="formGridQuery">
+                <Form.Label>Time</Form.Label>
+                <Form.Control onChange={this.handleChange} name="time" type="time" rows={3} required/>
+              </Form.Group>
+            </div>
+            <Button onClick={this.handleSubmit} variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
       </section>
     );
   }
